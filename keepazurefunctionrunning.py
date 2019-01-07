c99e0cf491838f9ad4aed722d41ecab1143dc1cf
Victor Santana <victor@hepoca.com>
2019-01-07 11:05:05 -0600

    update keep azure function running.

--- keepazurefunctionrunning.py
+++ keepazurefunctionrunning.py
@@ -6,5 +6,5 @@ import time
 while True:
     time.sleep(240)
     print(str(datetime.datetime.now()) + ": Calling Azure Function: https://vwsfunction.azurewebsites.net/api/alexapy")
-    requests.get("https://vwsfunction.azurewebsites.net/api/alexapy")
-    
+    r = requests.get("https://vwsfunction.azurewebsites.net/api/alexapy")
+    print(str(datetime.datetime.now()) + ": Http Result: " + str(r.status_code))
